0b3c619ae1f2c372f6bab19cabf7b129da45077a
Bruno Andrade <bruno.faria.andrade@gmail.com>
2022-08-06 21:24:31 -0300

    Added random objects

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ import { Sprite, Cube, Sphere, Coffin } from './display/index.js'
 
 const app = new Simulation('#stage')
 
-const object = new Coffin(new Vector(0, 0, 0), 100)
+const cube = new Cube(new Vector(0, 0, 0), 100)
+const coffin = new Coffin(new Vector(0, 0, 0), 100)
+const sphere = new Sphere(new Vector(0, 0, 0), 100)
+
+const objects = [
+    cube,
+    coffin,
+    sphere,
+]
+
+let object = objects[0]
 
 const Transform = new Matrix()
+
+setInterval(() => {
+    const rndIndex = random(objects.length) | 0
+    object = objects[rndIndex]
+}, 3000)
 
 app.fixedUpdate = (dt) => {
 
     object.yAngle += 0.7 * dt
+    object.xAngle += 0.7 * dt
 
     object.transform = Transform
 
